219c140ff1f5c245a8a35c6fcf392128ac871d40
JessieCheng <hamy820326@gmail.com>
2021-06-15 21:02:47 +0800

    install vee-validate lib

---
+++ src/views/Test.vue
@@ -0,0 +1,42 @@
+<template>
+  <h1>this is test page</h1>
+
+  <Form v-slot="{ errors, values }" @submit="onSubmit">
+    {{ errors }} {{ values }}
+    <div class="mb-3">
+      <label for="email" class="form-label">Email</label>
+
+      <Field
+        id="email"
+        name="email"
+        type="email"
+        class="form-control"
+        :class="{ 'is-invalid': errors['email'] }"
+        placeholder="請輸入 Email"
+        rules="email|required"
+        v-model="user.email"
+      ></Field>
+      <error-message name="email" class="invalid-feedback"></error-message>
+    </div>
+
+    <button class="btn btn-primary" type="submit">Submit</button>
+  </Form>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      user: {}
+    }
+  },
+  methods: {
+    onSubmit () {
+      console.log(this.user)
+    }
+  },
+  created () {
+    console.log(this)
+  }
+}
+</script>
