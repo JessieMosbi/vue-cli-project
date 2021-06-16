e69f9fa3797450b91b3a5fcff0a562aea8b85a64
JessieCheng <hamy820326@gmail.com>
2021-06-16 21:04:14 +0800

    css: add navbar

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,42 @@
 <template>
-  <div id="nav">
-    <router-link to="/products">產品列表</router-link> |
-    <router-link to="/carts">購物車</router-link> |
+  <nav class="navbar navbar-expand-lg sticky-top navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">懶人福音</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <router-link to="/products" class="nav-link">產品列表</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/carts" class="nav-link">購物車</router-link>
+          </li>
+        </ul>
+          <input
+            class="form-control me-2"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+          />
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+      </div>
+    </div>
+  </nav>
+
+  <div class="container">
+    <router-view />
   </div>
-  <router-view />
 </template>
 
 <style lang="scss">
